f6be3af804a2b44e843dfadddcadc1406f02a015
eddie94 <myeddie@naver.com>
2020-07-23 15:51:46 +0900

    IDIOT core server added

--- core.py
+++ core.py
@@ -1,5 +1,33 @@
-import socket
-import threading
-import time
+import asyncio
+from random import random
 
-host = "127.0.0.1"
+async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    print("handler called")
+    while True:
+        data = await reader.read(1024)
+
+        if not data:
+            break
+
+        peername = writer.get_extra_info('peername')
+        msg = data.decode("utf-8")
+
+        print("received: {} from {}".format(msg, peername))
+
+def main():
+    
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(handler, host="127.0.0.1", port=5001, loop=loop)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        server.close()
+        print("closing")
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
+
+if __name__ == "__main__":
+    print("server running")
+    main()
