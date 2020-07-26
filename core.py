c4d950260c6ee29621d0bf75448c94cc42e0ad7c
eddie94 <myeddie@naver.com>
2020-07-26 17:11:34 +0900

    header info added

--- core.py
+++ core.py
@@ -3,16 +3,34 @@ from random import random
 
 async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     print("handler called")
+
+    data = await reader.read(1024)
+
+    if not data:
+        return
+
+    peername = writer.get_extra_info('peername')
+    header = data.decode("utf-8")
+
+    print("received: {} from {}".format(header, peername))
+
+    header_info = header.split()
+
+    if len(header_info) != 3:
+        writer.write("0".encode("utf-8"))
+        print("header corrupted")
+        return
+
+    topic_name = header_info[0]
+    datatype = header_info[1]
+    queue_size = header_info[2]
+
+    writer.write("1".encode("utf-8"))
+
     while True:
         data = await reader.read(1024)
-
-        if not data:
-            break
-
-        peername = writer.get_extra_info('peername')
         msg = data.decode("utf-8")
-
-        print("received: {} from {}".format(msg, peername))
+        print(msg)
 
 def main():
     
